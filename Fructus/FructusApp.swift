3e069439df784ae7091606e0f1b5a0662e02f74b
Sithvothy <kiv@msn.com>
2022-08-03 08:52:34 +0700

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Sithvothy Infinity on 03/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
